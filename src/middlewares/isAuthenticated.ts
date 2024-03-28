349b5ddea9e02f395a8f807c6854786f1ea0d3dc
Luca <luca_carinhena@hotmail.com>
2024-03-28 16:42:43 -0300

    add middlewares

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload {
+  sub: string;
+}
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.SECRET_JWT) as Payload;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: err.message });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { ListUserController } from "./controllers/User/ListUserController";
 import { AuthUserController } from "./controllers/User/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle)
 router.post ("/session", new AuthUserController().handle)
-router.get('/users', new ListUserController().handle)
+router.get('/users', isAuthenticated, new ListUserController().handle)
 
 
 export { router };
