67a946be37bf63368bf99024414a8a238d58df8c
Luca <luca_carinhena@hotmail.com>
2024-04-01 15:29:18 -0300

    chore: add multer & upload config multer

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import { Router, Request, Response } from "express";
+import multer from 'multer'
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { DetailUserController } from "./controllers/User/DetailUserController";
 import { AuthUserController } from "./controllers/User/AuthUserController";
 import { CreateCategoryController } from "./controllers/Category/CreateCategoryController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { ListCategoryController } from "./controllers/Category/ListCategoryController";
+import { CreateProductController } from "./controllers/Product/CreateProductController";
+import uploadConfig from './config/multer'
 
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
@@ -15,5 +20,7 @@ router.get("/users", isAuthenticated, new DetailUserController().handle);
 
 router.post("/category", isAuthenticated,  new CreateCategoryController().handle)
 router.get("/category", isAuthenticated, new ListCategoryController().handle)
+
+router.post("/product", isAuthenticated, upload.single("file"), new CreateProductController().handle)
 
 export { router }
